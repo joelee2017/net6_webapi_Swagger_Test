da5403630aa37c169ba8d026997efa2d166823fe
Joe <Joe.Li@kgi.com>
2022-04-11 13:54:08 +0800

    增修

--- net6_webapi_Swagger_Test/net6_webapi_Swagger_Test/Middleware/SwaggerBasicAuthMiddleware.cs
+++ net6_webapi_Swagger_Test/net6_webapi_Swagger_Test/Middleware/SwaggerBasicAuthMiddleware.cs
@@ -10,7 +10,8 @@
     {
         if (context.Request.Path.StartsWithSegments("/swagger"))
         {
-            string authHeader = context.Request.Headers["Authorization"];
+            //string authHeader = context.Request.Headers["Authorization"];
+            string authHeader = context.Request.Headers.Authorization;
             if (authHeader != null && authHeader.StartsWith("Basic "))
             {
                 // Get the credentials from request header
@@ -19,15 +20,18 @@
                 var credentials = Encoding.UTF8.GetString(inBytes).Split(':');
                 var username = credentials[0];
                 var password = credentials[1];
+
+                string testPassword = "abcd" + DateTime.Now.ToString("MMdd");
                 // validate credentials
-                if (username.Equals("swagger")
-                  && password.Equals("swagger"))
+                if (username.Equals("abcd")
+                  && password.Equals(testPassword))
                 {
                     await next.Invoke(context).ConfigureAwait(false);
                     return;
                 }
             }
-            context.Response.Headers["WWW-Authenticate"] = "Basic";
+            //context.Response.Headers["WWW-Authenticate"] = "Basic";
+            context.Response.Headers.WWWAuthenticate = "Basic";
             context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
         }
         else
